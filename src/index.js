33435cf422803cffda0977a15ce2b99b180d6195
PolinaBel <120118029+pollyBe@users.noreply.github.com>
2024-08-04 16:54:09 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 module.exports = function reverse (n) {
+    let result = '';
+
+    if (n < 0) {
+      n = Math.abs(n);
+    }
   
+  let stringN = String(n);
+  
+  for (let i = 0; i < stringN.length; i++) {
+      
+      
+      if (stringN[i.length - 1] == 0) {
+          stringN[i.length - 1] = '';
+      }
+  
+      result = stringN[i] + result;    
+  }
+  
+  return Number(result.trim());
 }
